b339cd6a96302e22a456ebd5a242828d3325ea6f
justin3ong <jong2@77global.biz>
2022-08-10 11:47:28 +0800

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogItemComponent } from './modules/blog/components/blog-item/blog-item.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path: "blog",
+  component: BlogItemComponent
+}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
